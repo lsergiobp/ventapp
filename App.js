2f38ef86f1b8111d4262300abe73d3e2274baeca
Sergio <lsergiobp@gmail.com>
2021-09-03 20:28:12 -0400

    Added fields and calculations

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
   View,
   Text,
@@ -6,74 +6,172 @@ import {
   TextInput,
   StyleSheet,
 } from 'react-native';
-class Inputs extends Component {
-  state = {
-    height: '',
-    weight: '',
-    bmi: '',
-    BmiResult: '',
+function VentApp() {
+  const [fr, setFr] = useState(0);
+  const [volume, setVolume] = useState(0);
+  const [ers, setErs] = useState(0);
+  const [ie, setIe] = useState(0);
+  const [raw, setRaw] = useState(0);
+  const [plato, setPlato] = useState(0);
+  const [peep, setPeep] = useState(0);
+  const [pico, setPico] = useState(0);
+  const [flux, setFlux] = useState(0);
+
+  const handleFr = (fr) => {
+    setFr(fr);
   };
-  handleHeight = (text) => {
-    this.setState({ height: text });
+
+  const handleVolume = (fr) => {
+    setVolume(fr);
   };
-  handleWeight = (text) => {
-    this.setState({ weight: text });
+
+  const handlePlato = (plato) => {
+    setErs(plato);
   };
-  calculate = (height, weight) => {
-    //calculation
-    var result =
-      (parseFloat(weight) * 10000) / (parseFloat(height) * parseFloat(height));
-    result = result.toFixed(2);
-    //display result
-    this.setState({ bmi: result });
-    if (result < 18.5) {
-      this.setState({ BmiResult: 'Underweight' });
-    } else if (result >= 18.5 && result < 25) {
-      this.setState({ BmiResult: 'Normal weight' });
-    } else if (result >= 25 && result < 30) {
-      this.setState({ BmiResult: 'Overweight' });
-    } else if (result >= 30) {
-      this.setState({ BmiResult: 'Obese' });
-    } else {
-      alert('Incorrect Input!');
-      this.setState({ BmiResult: '' });
+
+  const handlePeep = (peep) => {
+    setErs(peep);
+  };
+
+  const handleErs = (fr) => {
+    setErs(fr);
+  };
+
+  const handleIe = (fr) => {
+    setIe(fr);
+  };
+
+  const handleRaw = (fr) => {
+    setRaw(fr);
+  };
+
+  const handlePico = (pico) => {
+    setRaw(pico);
+  };
+
+  const handleFlux = (flux) => {
+    setRaw(flux);
+  };
+
+  const calculateErs = () => {
+    if (volume != null && plato != null && peep != null) {
+      let ers = volume / (plato - peep);
+      setErs(ers);
     }
   };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>BMI Calculator</Text>
 
-        <Text style={styles.label}>Height</Text>
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid='transparent'
-          placeholder='Height (Cm)'
-          autoCapitalize='none'
-          onChangeText={this.handleHeight}
-        />
-        <Text style={styles.label}>Weight</Text>
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid='transparent'
-          placeholder='Weight (Kg)'
-          autoCapitalize='none'
-          onChangeText={this.handleWeight}
-        />
+  const calculateRaw = () => {
+    if (pico != null && plato != null && flux != null) {
+      let ers = (pico - plato) / flux;
+      setErs(raw);
+    }
+  };
 
-        <TouchableOpacity
-          style={styles.submitButton}
-          onPress={() => this.calculate(this.state.height, this.state.weight)}
-        >
-          <Text style={styles.submitButtonText}> Calculate </Text>
-        </TouchableOpacity>
-        <Text style={styles.output}>{this.state.bmi}</Text>
-        <Text style={styles.resultText}>{this.state.BmiResult}</Text>
-      </View>
-    );
-  }
+  const calculateVent = () => {
+    //calculation
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lenon Mechanical Power Calculator</Text>
+
+      <Text style={styles.label}>FR</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Frequência Respiratória'
+        autoCapitalize='none'
+        onChangeText={handleFr}
+      />
+
+      <Text style={styles.label}>Volume Corrente(L)</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Volume em litros'
+        autoCapitalize='none'
+        onChangeText={handleVolume}
+      />
+
+      <Text style={styles.label}>Plato</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Plato'
+        autoCapitalize='none'
+        onChangeText={handlePlato}
+      />
+
+      <Text style={styles.label}>PEEP</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='PEEP'
+        autoCapitalize='none'
+        onChangeText={handlePeep}
+      />
+
+      <Text style={styles.label}>ERS</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Elastância/Complacência'
+        autoCapitalize='none'
+        disabled
+        value={ers}
+      />
+
+      <Text style={styles.label}>I:E</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Relação Inspiratória/Respiratória'
+        autoCapitalize='none'
+        onChangeText={handleIe}
+      />
+
+      <Text style={styles.label}>Pico</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Pico'
+        autoCapitalize='none'
+        onChangeText={handlePico}
+      />
+
+      <Text style={styles.label}>Fluxo</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Fluxo'
+        autoCapitalize='none'
+        onChangeText={handleFlux}
+      />
+
+      <Text style={styles.label}>Raw</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid='transparent'
+        placeholder='Resistência'
+        autoCapitalize='none'
+        disabled
+        value={raw}
+      />
+
+      <TouchableOpacity
+        style={styles.submitButton}
+        onPress={() => calculateErs()}
+      >
+        <Text style={styles.submitButtonText}>Calculate Mechanical Power</Text>
+      </TouchableOpacity>
+      <Text style={styles.output}>{}</Text>
+      <Text style={styles.resultText}>{}</Text>
+    </View>
+  );
 }
-export default Inputs;
+
+export default VentApp;
+
 const styles = StyleSheet.create({
   container: {
     paddingTop: 23,
@@ -93,7 +191,6 @@ const styles = StyleSheet.create({
   submitButtonText: {
     textAlign: 'center',
     color: 'white',
-    // fontWeight:"bold",
     fontSize: 18,
   },
   output: {
